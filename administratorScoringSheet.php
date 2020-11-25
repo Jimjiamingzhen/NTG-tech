2a8241f35593f78dcac70c0b989447248a7c25f4
JIM <903657094@qq.com>
2020-11-25 20:54:32 +0800

    editable table

--- administratorScoringSheet.php
+++ administratorScoringSheet.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <!-- Check current user -->
+
 <?php 
         if (!session_id()){
             session_start();
@@ -10,14 +11,15 @@
             $evaluator =  $_SESSION['user'];
             if(isset($_SESSION['role'])){
                 if($_SESSION['role'] == 1){
-                    header("Location:scoringSheet.php");
+                    //header("Location:scoringSheet.php");
                 }
             }
         }
         else{
-            header("Location:login.php");
+            //header("Location:login.php");
         }
-    ?>
+?>
+
 
 <head>
     <meta charset="UTF-8">
@@ -63,6 +65,7 @@
         }
     </style>
     <script src="https://cdn.staticfile.org/vue/2.2.2/vue.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
 <body>
     <!-- HEAD -->   
@@ -80,10 +83,61 @@
     
     <!-- SCORING SHEET -->
     <div id = "scoringSheet">
+        <button @click = "console.log(evaluations);">ye</button>
+        <div v-for = "group in evaluations">
+            <table>
+                <tr>
+                    <th v-for = "person in group">
+                    {{person.evaluatee}}
+                    </th>
+                </tr>
+                <tr v-for = "i in 7">       <!-- rubricsnumber = 7, need to be modified later-->
+                    <td v-for = "person in group" v-Text="person.score[i-1]" contenteditable @input = "person.score[i-1]=$event.target.innerText;">
+                    </td>
+                    <!--
+                        在每个人后添加valid属性，是数组。再加一个总的valid确定是否可以被提交。  
+                        -->
+                </tr>
+            </table>
+        </div>
     </div>
 
     <script>
-    
+    new Vue({
+        el:"#scoringSheet",
+        data:{
+            evaluations:[],
+        },
+        methods:{
+            getEvaluatees:function(){
+                var evaluator = "<?php echo "贾明臻";?>"
+                var params = new URLSearchParams();
+                params.append('evaluator',evaluator);
+                var that = this;
+                axios
+                    .post('getGroupMembersName.php',params)
+                    .then(
+                        function(response){
+                            console.log(response);
+                            var evaluateeNames = response.data;
+                            for (group in evaluateeNames){
+                                groupEvaluations = new Array();
+                                for(person in evaluateeNames[group]){
+                                    groupEvaluations.push({evaluatee : evaluateeNames[group][person], score:[1,2,3,1,2,1,2], comment:""});
+                                }
+                                that.evaluations.push(groupEvaluations);
+                            } 
+                            console.log(that.evaluations);
+                        }
+
+                    );
+
+            }
+        },
+        created(){
+                this.getEvaluatees();
+            }
+        })
     </script>
 </body>
 </html>
