90fbfc4ff2bf2169b9f3a4afa73c5585596409c0
Jim <903657094@qq.com>
2020-11-30 18:49:02 +0800

    11.30 modified

--- administratorScoringSheet.php
+++ administratorScoringSheet.php
@@ -113,9 +113,6 @@
                         <th>{{rubrics[i-1]}}</th>
                         <td v-for = "person in group" v-Text="person.score[i-1]" contenteditable @input = "person.score[i-1]=$event.target.innerText;" :class = "{invalid: !person.valid[i-1]}">
                         </td>
-                        <!--
-                            在每个人后添加valid属性，是数组。再加一个总的valid确定是否可以被提交。  
-                            -->
                     </tr>
                     <tr>
                         <th>Comment</th>
@@ -129,136 +126,136 @@
     </div>
 
     <script>
-    new Vue({
-        el:"#scoringSheet",
-        data:{
-            evaluations:[],
-            week:"",
-            weekValid:true,
-            evaluator:"<?php echo "洪小平";?>",
-            date:"",
-            rubrics:['Knowledge Acquisition','Motivation','Communication','Hands-on Skills', 'Thinking Skills','Responsibility','Project Execution'],
-            course:"",
-            alertText:"啊哈",
-            errorCount:0,
-            submitAllowed:true
-        },
-        methods:{
-            getEvaluatees:function(){
-                var params = new URLSearchParams();
-                params.append('evaluator',this.evaluator);
-                var that = this;
-                axios
-                    .post('getGroupMembersName.php',params)
-                    .then(
-                        function(response){
-                            console.log(response);
-                            var evaluateeNames = response.data;
-                            for (group in evaluateeNames){
-                                groupEvaluations = new Array();
-                                for(person in evaluateeNames[group]){
-                                    groupEvaluations.push({evaluatee : evaluateeNames[group][person], score:[1,2,3,1,2,3,1], comment:"", valid:[true,true,true,true,true,true,true]});
-                                }
-                                that.evaluations.push(groupEvaluations);
-                            } 
-                            console.log(that.evaluations);
-                        }
-
-                    );
-                var date = new Date();
-                this.date = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-
+        new Vue({
+            el:"#scoringSheet",
+            data:{
+                evaluations:[],
+                week:"",
+                weekValid:true,
+                evaluator:"<?php echo "贾明臻";?>",
+                date:"",
+                rubrics:['Knowledge Acquisition','Motivation','Communication','Hands-on Skills', 'Thinking Skills','Responsibility','Project Execution'],
+                course:"",
+                alertText:"啊哈",
+                errorCount:0,
+                submitAllowed:true
             },
-            submitEvaluation:function(){
-                function Evaluation(week, evaluator, evaluatee, course, K, M, C, H, T, R, P, date,comment){
-                    this.week = week;
-                    this.evaluator = evaluator;
-                    this.evaluatee = evaluatee;
-                    this.course = course
-                    this.K = K;
-                    this.M = M;
-                    this.C = C;
-                    this.H = H;
-                    this.T = T;
-                    this.R = R;
-                    this.P = P;
-                    this.InputDate = date;
-                    this.comment = comment;
-                }
-                function validateScore(personRecord, rubricsNumber, that){
-                    invalid=[];
-                    for (var i = 0; i < rubricsNumber; i++){
-                        if(personRecord.score[i]<0 || personRecord.score[i]>5 || isNaN(personRecord.score[i])){
+            methods:{
+                getEvaluatees:function(){
+                    var params = new URLSearchParams();
+                    params.append('evaluator',this.evaluator);
+                    var that = this;
+                    axios
+                        .post('getGroupMembersName.php',params)
+                        .then(
+                            function(response){
+                                console.log(response);
+                                var evaluateeNames = response.data;
+                                for (group in evaluateeNames){
+                                    groupEvaluations = new Array();
+                                    for(person in evaluateeNames[group]){
+                                        groupEvaluations.push({evaluatee : evaluateeNames[group][person], score:[1,2,3,1,2,3,1], comment:"", valid:[true,true,true,true,true,true,true]});
+                                    }
+                                    that.evaluations.push(groupEvaluations);
+                                } 
+                                console.log(that.evaluations);
+                            }
+
+                        );
+                    var date = new Date();
+                    this.date = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+
+                },
+                submitEvaluation:function(){
+                    function Evaluation(week, evaluator, evaluatee, course, K, M, C, H, T, R, P, date,comment){
+                        this.week = week;
+                        this.evaluator = evaluator;
+                        this.evaluatee = evaluatee;
+                        this.course = course
+                        this.K = K;
+                        this.M = M;
+                        this.C = C;
+                        this.H = H;
+                        this.T = T;
+                        this.R = R;
+                        this.P = P;
+                        this.InputDate = date;
+                        this.comment = comment;
+                    }
+                    function validateScore(personRecord, rubricsNumber, that){
+                        invalid=[];
+                        for (var i = 0; i < rubricsNumber; i++){
+                            if(personRecord.score[i]<0 || personRecord.score[i]>5 || isNaN(personRecord.score[i])){
+                                that.submitAllowed = false;
+                                invalid.push(i);
+                                that.errorCount+=1;
+                                alert = '<br>error' + that.errorCount + ': Score should be numbers between 0 and 5';
+                                that.alertText+=alert;
+                            }
+                            else{
+                                personRecord.valid[i]= true;
+                            }
+                        }
+                        return invalid;
+                    }
+
+                    function validateWeek(week, that){
+                        if(week<=0 || week>16 || isNaN(week) || Math.floor(week) != week){
                             that.submitAllowed = false;
-                            invalid.push(i);
+                            that.weekValid = false;
                             that.errorCount+=1;
-                            alert = '<br>error' + that.errorCount + ': Score should be numbers between 0 and 5';
+                            alert = '<br>error' + that.errorCount + ': week should be a number between 0 and 16';
                             that.alertText+=alert;
                         }
-                        else{
-                            personRecord.valid[i]= true;
+
+
+                    }
+
+                    var evaluationsToSubmit = [];
+                    var evaluations = this.evaluations;
+                    var that = this;
+                    this.errorCount = 0;
+                    this.alertText = "";
+                    this.submitAllowed = true;
+                    this.weekValid = true;
+                    validateWeek(this.week, that);
+                    for(group in evaluations){
+                        for(person in evaluations[group]){
+                            var personRecord = evaluations[group][person];
+                            invalid = validateScore(personRecord,7,that);
+                            for(i in invalid){
+                                this.evaluations[group][person].valid[invalid[i]] = false;
+                                this.$forceUpdate();
+                            }
+                            evaluationsToSubmit.push(
+                                new Evaluation(
+                                    this.week, this.evaluator, personRecord.evaluatee, 
+                                    this.course, personRecord.score[0], personRecord.score[1], 
+                                    personRecord.score[2], personRecord.score[3], personRecord.score[4], 
+                                    personRecord.score[5], personRecord.score[6], this.date, personRecord.comment));
                         }
                     }
-                    return invalid;
-                }
-
-                function validateWeek(week, that){
-                    if(week<=0 || week>16 || isNaN(week) || Math.floor(week) != week){
-                        that.submitAllowed = false;
-                        that.weekValid = false;
-                        that.errorCount+=1;
-                        alert = '<br>error' + that.errorCount + ': week should be a number between 0 and 16';
-                        that.alertText+=alert;
+                    console.log(this.submitAllowed);
+                    if (this.submitAllowed == true){
+                        var evaluationsInJson = JSON.stringify(evaluationsToSubmit)
+                        $.post("handleEvaluations.php",{
+                            evaluationData:evaluationsInJson
+                        },
+                        function(data, status){
+                            alert(data);
+                            this.alertText += data;
+                        });
+                    }
+                    else{
+                        this.alertText += '<br>Submit fail';
                     }
 
-
                 }
-
-                var evaluationsToSubmit = [];
-                var evaluations = this.evaluations;
-                var that = this;
-                this.errorCount = 0;
-                this.alertText = "";
-                this.submitAllowed = true;
-                this.weekValid = true;
-                validateWeek(this.week, that);
-                for(group in evaluations){
-                    for(person in evaluations[group]){
-                        var personRecord = evaluations[group][person];
-                        invalid = validateScore(personRecord,7,that);
-                        for(i in invalid){
-                            this.evaluations[group][person].valid[invalid[i]] = false;
-                            this.$forceUpdate();
-                        }
-                        evaluationsToSubmit.push(
-                            new Evaluation(
-                                this.week, this.evaluator, personRecord.evaluatee, 
-                                this.course, personRecord.score[0], personRecord.score[1], 
-                                personRecord.score[2], personRecord.score[3], personRecord.score[4], 
-                                personRecord.score[5], personRecord.score[6], this.date, personRecord.comment));
-                    }
+            },
+            created(){
+                    this.getEvaluatees();
                 }
-                console.log(this.submitAllowed);
-                if (this.submitAllowed == true){
-                    var evaluationsInJson = JSON.stringify(evaluationsToSubmit)
-                    $.post("handleEvaluations.php",{
-                        evaluationData:evaluationsInJson
-                    },
-                    function(data, status){
-                        alert(data);
-                        this.alertText += data;
-                    });
-                }
-                else{
-                    this.alertText += '<br>Submit fail';
-                }
-
-            }
-        },
-        created(){
-                this.getEvaluatees();
-            }
-        })
+            })
     </script>
 </body>
 </html>
